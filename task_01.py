6a1b18323e941c47ed6695707ba22a498a454a3f
Sarah Dignardi <sarahmleavy@gmail.com>
2014-09-14 22:32:34 -0400

    re-add task 1

--- task_01.py
+++ task_01.py
@@ -3,8 +3,8 @@
 """Task 01: Collecting User Input"""
 
 QUESTION_01 = raw_input('What is your name?')
-print(QUESTION_01)
+print QUESTION_01
 
 QUESTION_02 = raw_input('How old are you?')
 QUESTION_02 = int(QUESTION_02)
-print(QUESTION_02)
+print QUESTION_02
