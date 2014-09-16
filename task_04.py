26e09b295064faba9a0c473a247ba0b9cf2322c6
Sarah Dignardi <sarahmleavy@gmail.com>
2014-09-15 23:03:23 -0400

    4x a charm?

--- task_04.py
+++ task_04.py
@@ -5,4 +5,4 @@
 DAY = raw_input('What day is it?').lower()
 TIME = raw_input('What time is it? (e.g. 2300)')
 SNOOZE = 'True' if DAY == 'saturday' or DAY == 'sunday' or TIME < 0600 \
-else 'False'
+    else 'False'
