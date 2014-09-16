43acd92b600073150c8b428c9572346c96d5e0aa
Sarah Dignardi <sarahmleavy@gmail.com>
2014-09-15 20:29:30 -0400

    task 2 x4

--- task_02.py
+++ task_02.py
@@ -4,13 +4,13 @@
 
 BP_STATUS = int(raw_input('What is your systolic blood pressure?'))
 
-if BP_STATUS <= 90: 
-    print 'Hello, your blood pressure status is low.'
-elif BP_STATUS > 90 and BP_STATUS >= 119: 
-    print 'Hello, your blood pressure status is ideal.'
-elif BP_STATUS > 119 and BP_STATUS <= 139: 
-    print 'Hello, your blood pressure status is warning.'
-elif BP_STATUS > 139 and BP_STATUS <= 160: 
-    print 'Hello, your blood pressure status is high.'
-elif BP_STATUS > 160: 
-    print 'Hello, your blood pressure status is emergency.'
+if BP_STATUS <= 90:
+    print'Hello, your blood pressure status is low.'
+elif BP_STATUS > 90 and BP_STATUS >= 119:
+    print'Hello, your blood pressure status is ideal.'
+elif BP_STATUS > 119 and BP_STATUS <= 139:
+    print'Hello, your blood pressure status is warning.'
+elif BP_STATUS > 139 and BP_STATUS <= 160:
+    print'Hello, your blood pressure status is high.'
+elif BP_STATUS > 160:
+    print'Hello, your blood pressure status is emergency.'
