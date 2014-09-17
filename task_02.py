4eb72d9773bfb098124e325b705f5570ca060781
Sarah Dignardi <sarahmleavy@gmail.com>
2014-09-17 19:28:23 -0400

    add task 2 again

--- task_02.py
+++ task_02.py
@@ -2,15 +2,19 @@
 # -*- coding: utf-8 -*-
 """Task 02: Simple Branching"""
 
-BP_STATUS = int(raw_input('What is your systolic blood pressure?'))
+BP_INPUT = int(raw_input('What is your systolic blood pressure?'))
+print BP_INPUT
+BP_STATUS = int()
 
-if BP_STATUS <= 90:
-    print'Hello, your blood pressure status is low.'
-elif BP_STATUS > 90 and BP_STATUS >= 119:
-    print'Hello, your blood pressure status is ideal.'
-elif BP_STATUS > 119 and BP_STATUS <= 139:
-    print'Hello, your blood pressure status is warning.'
-elif BP_STATUS > 139 and BP_STATUS <= 160:
-    print'Hello, your blood pressure status is high.'
-elif BP_STATUS > 160:
-    print'Hello, your blood pressure status is emergency.'
+if BP_INPUT <= 90:
+    BP_STATUS = 'low'
+elif BP_INPUT > 90 and BP_INPUT <= 119:
+    BP_STATUS = 'ideal'
+elif BP_INPUT > 119 and BP_INPUT <= 139:
+    BP_STATUS = 'warning'
+elif BP_INPUT > 139 and BP_INPUT <= 160:
+    BP_STATUS = 'high'
+elif BP_INPUT > 160:
+    BP_STATUS = 'emergency'
+
+print'Hello, your blood pressure status is {}.'.format(BP_STATUS)
