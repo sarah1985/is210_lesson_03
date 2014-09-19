5b9b816216617a5b3e18a1112c744e5170f79f5c
Sarah Dignardi <sarahmleavy@gmail.com>
2014-09-19 17:29:28 -0400

    re-add task 5

--- task_05.py
+++ task_05.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 """Task 05: Compound Examples"""
 
+from decimal import *
+import locale
+locale.setlocale(locale.LC_ALL, '')
 NAME = raw_input('What is your name?')
 PRINCIPAL = int(raw_input('What is the amount of your principal (the amount being borrowed)?'))
 TERM = int(raw_input('For how many years is this loan being borrowed?'))
@@ -11,59 +14,59 @@ INT = 0
 if 0 <= PRINCIPAL <= 199999:
 	if 1 <= TERM <= 15:
 		if PRE_QUAL == 'Yes':
-			INT = float(0.0363)
-		else PRE_QUAL == 'No':
-			INT = float(0.0465)
+			INT = Decimal(0.0363)
+		else:
+			INT = Decimal(0.0465)
 	elif 15 < TERM <= 20:
 		if PRE_QUAL == 'Yes':
-			INT = float(0.0404)
-		else PRE_QUAL == 'No':
-			INT = float(0.0498)
+			INT = Decimal(0.0404)
+		else:
+			INT = Decimal(0.0498)
 	elif 20 < TERM <= 30:
 		if PRE_QUAL == 'Yes':
-			INT = float(0.0577)
-		else PRE_QUAL == 'No':
-			INT = float(0.0639)
+			INT = Decimal(0.0577)
+		else:
+			INT = Decimal(0.0639)
 if 200000 <= PRINCIPAL <= 999999:
 	if 1 <= TERM <= 15:
 		if PRE_QUAL == 'Yes': 
-			INT = float(0.0302)
-		else PRE_QUAL == 'No':
-			INT = float(0.0398)
+			INT = Decimal(0.0302)
+		else:
+			INT = Decimal(0.0398)
 	elif 15 < TERM <= 20:
 		if PRE_QUAL == 'Yes':
-			INT = float(0.0327)
-		else PRE_QUAL == 'No':
-			INT = float(0.0408)
+			INT = Decimal(0.0327)
+		else:
+			INT = Decimal(0.0408)
 	elif 20 < TERM <= 30:
 		if PRE_QUAL == 'Yes':
-			INT = float(0.0466)
-		else PRE_QUAL == 'No':
+			INT = Decimal(0.0466)
+		else:
 			INT = NULL
 			print 'None'
 if PRINCIPAL > 1000000:
 	if 1 <= TERM <= 15:
 		if PRE_QUAL == 'Yes':
-			INT = float(0.0205)
-		else PRE_QUAL == 'No':
+			INT = Decimal(0.0205)
+		else:
 			INT = NULL
 			print 'None'
 	elif 15 < TERM <= 20:
 		if PRE_QUAL == 'Yes':
-			INT = float(0.0262)
-		else PRE_QUAL == 'No':
+			INT = Decimal(0.0262)
+		else:
 			INT = NULL
 			print 'None'
 
-TOTAL = round(PRINCIPAL * [1 + (INT/12)]**(12 * TERM))
+TOTAL = round(PRINCIPAL * (1 + (INT/12))**(12 * TERM))
 
 REPORT = '''
-Loan Report for: {}
+Loan Report for: {0}
 -------------------------------------------------------------
-	Principal:			${}
-	Duration:			{} yrs
-	Pre-qualified?		{}
+	Principal:       {1:>15}
+	Duration:        {2:>15}
+	Pre-qualified?   {3:>15}
 	
-	Total:				${}'''.format(NAME, PRINCIPAL, DURATION, PRE_QUAL)
+	Total:           {4:>15}'''.format(NAME, locale.currency(PRINCIPAL, grouping=True), str(TERM) + 'yrs', PRE_QUAL, locale.currency(TOTAL, grouping=True))
 
 print REPORT
