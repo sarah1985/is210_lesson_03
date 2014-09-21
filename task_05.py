6873b547afe85dac7b912a3a8d71dec9a90b47d4
Sarah Dignardi <sarahmleavy@gmail.com>
2014-09-21 16:29:40 -0400

    adding 5 again

--- task_05.py
+++ task_05.py
@@ -12,62 +12,62 @@ PRE_QUAL = raw_input('Are you pre-qualified for this loan?')
 INT = 0
 
 if 0 <= PRINCIPAL <= 199999:
-	if 1 <= TERM <= 15:
-		if PRE_QUAL == 'Yes':
-			INT = Decimal('0.0363')
-		else:
-			INT = Decimal('0.0465')
-	elif 15 < TERM <= 20:
-		if PRE_QUAL == 'Yes':
-			INT = Decimal('0.0404')
-		else:
-			INT = Decimal('0.0498')
-	elif 20 < TERM <= 30:
-		if PRE_QUAL == 'Yes':
-			INT = Decimal('0.0577')
-		else:
-			INT = Decimal('0.0639')
+    if 1 <= TERM <= 15:
+        if PRE_QUAL == 'Yes':
+            INT = Decimal('0.0363')
+        else:
+            INT = Decimal('0.0465')
+    elif 15 < TERM <= 20:
+        if PRE_QUAL == 'Yes':
+            INT = Decimal('0.0404')
+        else:
+            INT = Decimal('0.0498')
+    elif 20 < TERM <= 30:
+        if PRE_QUAL == 'Yes':
+            INT = Decimal('0.0577')
+        else:
+            INT = Decimal('0.0639')
 if 200000 <= PRINCIPAL <= 999999:
-	if 1 <= TERM <= 15:
-		if PRE_QUAL == 'Yes': 
-			INT = Decimal('0.0302')
-		else:
-			INT = Decimal('0.0398')
-	elif 15 < TERM <= 20:
-		if PRE_QUAL == 'Yes':
-			INT = Decimal('0.0327')
-		else:
-			INT = Decimal('0.0408')
-	elif 20 < TERM <= 30:
-		if PRE_QUAL == 'Yes':
-			INT = Decimal('0.0466')
-		else:
-			INT = None
-			print 'None'
+    if 1 <= TERM <= 15:
+        if PRE_QUAL == 'Yes': 
+            INT = Decimal('0.0302')
+        else:
+            INT = Decimal('0.0398')
+    elif 15 < TERM <= 20:
+        if PRE_QUAL == 'Yes':
+            INT = Decimal('0.0327')
+        else:
+            INT = Decimal('0.0408')
+    elif 20 < TERM <= 30:
+        if PRE_QUAL == 'Yes':
+            INT = Decimal('0.0466')
+        else:
+            INT = None
 if PRINCIPAL > 1000000:
-	if 1 <= TERM <= 15:
-		if PRE_QUAL == 'Yes':
-			INT = Decimal('0.0205')
-		else:
-			INT = None
-			print 'None'
-	elif 15 < TERM <= 20:
-		if PRE_QUAL == 'Yes':
-			INT = Decimal('0.0262')
-		else:
-			INT = None
-			print 'None'
+    if 1 <= TERM <= 15:
+        if PRE_QUAL == 'Yes':
+            INT = Decimal('0.0205')
+        else:
+            INT = None
+    elif 15 < TERM <= 20:
+        if PRE_QUAL == 'Yes':
+            INT = Decimal('0.0262')
+        else:
+            INT = None
 
-TOTAL = round(PRINCIPAL * (1 + (INT/12))**(12 * TERM))
+if INT != None:
+    TOTAL = round(PRINCIPAL * (1 + (INT/12))**(12 * TERM))
+else:
+    TOTAL = 0
 
 REPORT = '''
 Loan Report for: {0}
 -------------------------------------------------------------
-	Principal:       {1:>15}
-	Duration:        {2:>15}
-	Pre-qualified?   {3:>15}
-	
-	Total:           {4:>15}'''.format(NAME, locale.currency(PRINCIPAL, grouping=True), 
+    Principal:       {1:>15}
+    Duration:        {2:>15}
+    Pre-qualified?   {3:>15}
+    
+    Total:           {4:>15}'''.format(NAME, locale.currency(PRINCIPAL, grouping=True), 
 str(TERM) + 'yrs', PRE_QUAL, locale.currency(TOTAL, grouping=True))
 
 print REPORT
