470f2610f5e48af6f3fe40baf4b00a2f347273fc
Sarah Dignardi <sarahmleavy@gmail.com>
2014-09-21 15:28:51 -0400

    add task 3

--- task_03.py
+++ task_03.py
@@ -7,46 +7,46 @@ ACCENT = 0
 HIGHLIGHT = 0
 
 if BASE == 'seattle gray':
-	ACCENT = raw_input('Do you prefer Ceramic Glaze or Cumulus Nimbus?').lower()
-	if ACCENT == 'ceramic glaze':
-		HIGHLIGHT = raw_input('Do you prefer Basically White or White?').lower()
-		if HIGHLIGHT == 'basically white':
-			print BASE
-			print ACCENT
-			print HIGHLIGHT
-		elif HIGHLIGHT == 'white':
-			print BASE
-			print ACCENT
-			print HIGHLIGHT
-	elif ACCENT == 'cumulus nimbus':
-		HIGHLIGHT = raw_input('Do you prefer Basically White or White?').lower()
-		if HIGHLIGHT == 'basically white':
-			print BASE
-			print ACCENT
-			print HIGHLIGHT
-		elif HIGHLIGHT == 'white':
-			print BASE
-			print ACCENT
-			print HIGHLIGHT
+    ACCENT = raw_input('Do you prefer Ceramic Glaze or Cumulus Nimbus?').lower()
+    if ACCENT == 'ceramic glaze':
+        HIGHLIGHT = raw_input('Do you prefer Basically White or White?').lower()
+        if HIGHLIGHT == 'basically white':
+            print BASE
+            print ACCENT
+            print HIGHLIGHT
+        elif HIGHLIGHT == 'white':
+            print BASE
+            print ACCENT
+            print HIGHLIGHT
+    elif ACCENT == 'cumulus nimbus':
+        HIGHLIGHT = raw_input('Do you prefer Basically White or White?').lower()
+        if HIGHLIGHT == 'basically white':
+            print BASE
+            print ACCENT
+            print HIGHLIGHT
+        elif HIGHLIGHT == 'white':
+            print BASE
+            print ACCENT
+            print HIGHLIGHT
 elif BASE == 'Manatee':
-	ACCENT = raw_input('Do you prefer Platinum Mist or Spartan Sage?').lower()
-	if ACCENT == 'platinum mist':
-		HIGHLIGHT = raw_input('Do you prefer Bone White or Just White?').lower()
-		if HIGHLIGHT == 'bone white':
-			print BASE
-			print ACCENT
-			print HIGHLIGHT
-		elif HIGHLIGHT == 'just white':
-			print BASE
-			print ACCENT
-			print HIGHLIGHT
-	elif ACCENT == 'spartan sage':
-		HIGHLIGHT = raw_input('Do you prefer Fractal White or Not White?').lower()
-		if HIGHLIGHT == 'fractal white':
-			print BASE
-			print ACCENT
-			print HIGHLIGHT
-		elif HIGHLIGHT == 'not white':
-			print BASE
-			print ACCENT
-			print HIGHLIGHT
+    ACCENT = raw_input('Do you prefer Platinum Mist or Spartan Sage?').lower()
+    if ACCENT == 'platinum mist':
+        HIGHLIGHT = raw_input('Do you prefer Bone White or Just White?').lower()
+        if HIGHLIGHT == 'bone white':
+            print BASE
+            print ACCENT
+            print HIGHLIGHT
+        elif HIGHLIGHT == 'just white':
+            print BASE
+            print ACCENT
+            print HIGHLIGHT
+    elif ACCENT == 'spartan sage':
+        HIGHLIGHT = raw_input('Do you prefer Fractal White or Not White?').lower()
+        if HIGHLIGHT == 'fractal white':
+            print BASE
+            print ACCENT
+            print HIGHLIGHT
+        elif HIGHLIGHT == 'not white':
+            print BASE
+            print ACCENT
+            print HIGHLIGHT
