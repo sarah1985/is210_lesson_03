6245b063eecf2800cf1fc5d56c33e878f924e24a
Sarah Dignardi <sarahmleavy@gmail.com>
2014-09-23 23:42:39 -0400

    5

--- task_05.py
+++ task_05.py
@@ -70,9 +70,9 @@ Loan Report for: {0}
     Principal:       {1:>15}
     Duration:        {2:>15}
     Pre-qualified?   {3:>15}
-    
+
     Total:           {4:>15}'''.format(
-    NAME, 
+    NAME,
     locale.currency(PRINCIPAL, grouping=True),
     str(TERM) + 'yrs',
     PRE_QUAL,
